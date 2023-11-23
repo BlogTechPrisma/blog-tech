cd430860de56da5527fa2147c02e343eb090a8b0
Vinícius Moreira Henrique <vinmoreira@pop-os.localdomain>
2023-11-23 15:38:09 -0300

    fix: error that id column was on the last position of the table

--- prisma/migrations/20231123183625_initial_migration/migration.sql
+++ prisma/migrations/20231123183625_initial_migration/migration.sql
@@ -1,10 +1,11 @@
 -- CreateTable
 CREATE TABLE "Users" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "full_name" VARCHAR(25) NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
